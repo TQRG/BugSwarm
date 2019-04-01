9bd8a6ad71204cd24e4d4590848065be8cd031a5
tdurieux <durieuxthomas@gmail.com>
2019-04-01 21:29:59 +0000

    Buggy version of stagemonitor-stagemonitor-126992617

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Any subsequent commands which fail will cause the shell script to exit immediately
+set -e
+
+./gradlew clean release
+
+git push
+
+# check out the tag that was just created
+git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+
+./gradlew uploadArchives
+
+git checkout master
---
+++ stagemonitor-core/src/test/java/org/stagemonitor/core/StagemonitorCoreConfigurationSourceInitializerTest.java
@@ -0,0 +1,47 @@
+package org.stagemonitor.core;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.stagemonitor.core.configuration.Configuration;
+import org.stagemonitor.core.configuration.source.ConfigurationSource;
+import org.stagemonitor.core.elasticsearch.ElasticsearchClient;
+
+public class StagemonitorCoreConfigurationSourceInitializerTest {
+
+	private StagemonitorCoreConfigurationSourceInitializer initializer = new StagemonitorCoreConfigurationSourceInitializer();
+	final Configuration configuration = Mockito.mock(Configuration.class);
+	final CorePlugin corePlugin = Mockito.mock(CorePlugin.class);
+
+	@Before
+	public void setUp() throws Exception {
+		when(corePlugin.getElasticsearchConfigurationSourceProfiles()).thenReturn(Arrays.asList("test"));
+		when(corePlugin.getThreadPoolQueueCapacityLimit()).thenReturn(1000);
+		when(configuration.getConfig(CorePlugin.class)).thenReturn(corePlugin);
+		ElasticsearchClient elasticsearchClient = new ElasticsearchClient(corePlugin);
+		when(corePlugin.getElasticsearchClient()).thenReturn(elasticsearchClient);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testEsDownDeactivate() throws Exception {
+		when(corePlugin.isDeactivateStagemonitorIfEsConfigSourceIsDown()).thenReturn(true);
+
+		initializer.onConfigurationInitialized(new StagemonitorConfigurationSourceInitializer.ConfigInitializedArguments(configuration));
+	}
+
+	@Test
+	public void testEsDown() throws Exception {
+		when(corePlugin.isDeactivateStagemonitorIfEsConfigSourceIsDown()).thenReturn(false);
+
+		initializer.onConfigurationInitialized(new StagemonitorConfigurationSourceInitializer.ConfigInitializedArguments(configuration));
+
+		verify(configuration).addConfigurationSource(any(ConfigurationSource.class), eq(false));
+	}
+}
