9e06060462bfed3434b97599738acaf237944073
tdurieux <durieuxthomas@gmail.com>
2019-04-01 21:13:22 +0000

    Buggy version of davidmoten-rxjava-jdbc-65230626

---
+++ src/main/java/com/github/davidmoten/rx/jdbc/ConnectionProviderAutoCommitting.java
@@ -0,0 +1,42 @@
+package com.github.davidmoten.rx.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Provides {@link Connection}s with autoCommit set to true.
+ */
+final class ConnectionProviderAutoCommitting implements ConnectionProvider {
+
+    /**
+     * Underlying connection provider.
+     */
+    private final ConnectionProvider cp;
+
+    /**
+     * Constructor.
+     * 
+     * @param cp
+     *            underlying connection provider
+     */
+    ConnectionProviderAutoCommitting(ConnectionProvider cp) {
+        this.cp = cp;
+    }
+
+    @Override
+    public Connection get() {
+        Connection con = cp.get();
+        try {
+            con.setAutoCommit(true);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
+
+    @Override
+    public void close() {
+        cp.close();
+    }
+
+}
