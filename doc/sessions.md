1210a07aaf47a62ff10ab64ba31ff9c3a8b24ce7
tdurieux <durieuxthomas@gmail.com>
2019-04-02 16:43:02 +0000

    Buggy version of hivesolutions-appier-142245637

---
+++ doc/sessions.md
@@ -0,0 +1,79 @@
+# Sessions
+
+The session object can be acessed through [Models](models.md) or [Controllers](controllers.md)
+in the same way. Here's how you'd set the email of a logged in user in the session:
+
+```python
+self.session["email"] = email
+```
+
+Once set in the session, this value will be accessible by the same client across requests:
+
+```python
+email = self.session["email"]
+```
+
+You can access the session object in directly in any template as well:
+
+```html
+<p>Logged in as {{ session.email }}</p>
+```
+
+If you want to remove the variable from the session just do:
+
+```python
+del self.session["email"]
+```
+
+## Persistence
+
+The way a session is persisted is configurable, it can be stored in memory, a file, or a database, for example. To configure the type of session persistence to use, pass the `session_c` keyword in the app initialization:
+
+```python
+import appier
+
+class HelloApp(appier.App):
+
+    def __init__(self):
+        appier.App.__init__(
+            self,
+            session_c = appier.session.FileSession
+        )
+
+HelloApp().serve()
+```
+
+The following session types are currently available:
+
+* `MemorySession` - stores session values in memory (session is lost when app is stopped and relaunched)
+* `FileSession` - stores values in a [Shelve](https://docs.python.org/library/shelve.html) named `session.shelve.db` in the app root directory (session is kept even if the app is stopped and relaunched)
+* `RedisSession` - stores values in a [Redis](http://redis.io/) server should be used together with the `REDISTOGO_URL` configuration variable
+* `ClientSession` - stores values using cookies on the client, this is considered safe (message signed) avoid tampering from the client side
+
+## Transient
+
+To hold the session variables only for the duration of the request's lifecycle (eg: for authenticating the request with an oauth token), transient session variables can be set instead. These act just like regular session variables, therefore leveraging every other session-dependent feature for the duration of that request.
+
+To set a transient session value:
+
+```python
+self.session.set_t("email", "email")
+```
+
+To retrieve it later on you can just to a regular session value retrieval:
+
+```python
+email = self.session["email"]
+```
+
+Or you may want to explicitly state that you are looking for a transient value (will not retrieve regular values):
+
+```python
+self.session.get_t("email")
+```
+
+In case you want to delete it (not necessary in most cases, as it will be discarded at the end of the request):
+
+```python
+self.session.delete_t("email")
+```
---
+++ examples/async/async.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Appier Framework
+# Copyright (c) 2008-2016 Hive Solutions Lda.
+#
+# This file is part of Hive Appier Framework.
+#
+# Hive Appier Framework is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Appier Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import netius
+import mimetypes
+import threading
+
+import appier
+
+class AsyncApp(appier.App):
+
+    def __init__(self, *args, **kwargs):
+        appier.App.__init__(
+            self,
+            name = "async",
+            *args, **kwargs
+        )
+
+    @appier.route("/async", "GET")
+    def async(self):
+        yield -1
+        yield "before\n"
+        yield netius.ensure(self.handler)
+        yield "after\n"
+
+    @appier.route("/async_file", "GET")
+    def async_file(self):
+        file_path = self.field("path", None)
+        thread = self.field("thread", False, cast = bool)
+        type, _encoding = mimetypes.guess_type(file_path, strict = True)
+        type = type or "application/octet-stream"
+        self.request.content_type = type
+        yield -1
+        yield netius.ensure(
+            self.read_file,
+            args = [file_path],
+            thread = thread
+        )
+
+    @netius.coroutine
+    def handler(self, future):
+        thread = threading.current_thread()
+        print("executing in %s" % thread)
+        message = "hello world\n"
+        timeout = yield from netius.sleep(3.0)
+        message += "timeout: %.2f\n" % timeout
+        result = yield from self.calculator(2, 2)
+        message += "result: %d\n" % result
+        future.set_result(message)
+
+    @netius.coroutine
+    def calculator(self, *args, **kwargs):
+        thread = threading.current_thread()
+        print("executing in %s" % thread)
+        print("computing...")
+        yield from netius.sleep(3.0)
+        print("finished computing...")
+        return sum(args)
+
+    @netius.coroutine
+    def read_file(self, future, file_path, chunk = 65536, delay = 0):
+        count = 0
+        file = open(file_path, "rb")
+        try:
+            while True:
+                data = file.read(chunk)
+                if not data: break
+                count += len(data)
+                if delay: yield from netius.sleep(delay)
+                yield data
+        finally:
+            file.close()
+        future.set_result(None)
+        return count
+
+app = AsyncApp()
+app.serve()
