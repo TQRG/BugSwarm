c1928da9517d8cb5cd3e9e9a55b4a69456abf867
tdurieux <durieuxthomas@gmail.com>
2019-04-03 17:55:23 +0000

    Buggy version of HubSpot-Baragon-79555886

---
+++ BaragonData/src/main/java/com/hubspot/baragon/utils/JavaUtils.java
@@ -0,0 +1,42 @@
+package com.hubspot.baragon.utils;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+import com.google.common.base.Joiner;
+
+public class JavaUtils {
+  public static final Joiner COMMA_JOINER = Joiner.on(", ").skipNulls();
+
+  private JavaUtils() { }
+
+  public static String getHostAddress() throws Exception {
+    final Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+    while (interfaces.hasMoreElements()) {
+      final NetworkInterface current = interfaces.nextElement();
+
+      if (!current.isUp() || current.isLoopback() || current.isVirtual()) {
+        continue;
+      }
+
+      final Enumeration<InetAddress> addresses = current.getInetAddresses();
+
+      while (addresses.hasMoreElements()) {
+        final InetAddress current_addr = addresses.nextElement();
+
+        if (current_addr.isLoopbackAddress()) {
+          continue;
+        }
+
+        if (current_addr instanceof Inet4Address) {
+          return current_addr.getHostAddress();
+        }
+      }
+    }
+
+    throw new RuntimeException("Couldn't deduce host address");
+  }
+}
