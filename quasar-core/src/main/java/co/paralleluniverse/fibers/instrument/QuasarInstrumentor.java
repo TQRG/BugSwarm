21baa0fce8dd7252f76b54184662cddb7f7d442e
tdurieux <durieuxthomas@gmail.com>
2019-04-03 19:12:29 +0000

    Buggy version of puniverse-quasar-130412591

---
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/QuasarInstrumentor.java
@@ -0,0 +1,212 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2013-2016, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.fibers.instrument;
+
+import co.paralleluniverse.common.reflection.ReflectionUtil;
+import co.paralleluniverse.common.util.Debug;
+import co.paralleluniverse.common.util.SystemProperties;
+import co.paralleluniverse.fibers.instrument.MethodDatabase.WorkListEntry;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.util.CheckClassAdapter;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+
+/**
+ * @author pron
+ */
+public final class QuasarInstrumentor {
+    public static final int ASMAPI = Opcodes.ASM5;
+    private final static String EXAMINED_CLASS = System.getProperty("co.paralleluniverse.fibers.writeInstrumentedClassesStartingWith");
+    private static final boolean allowJdkInstrumentation = SystemProperties.isEmptyOrTrue("co.paralleluniverse.fibers.allowJdkInstrumentation");
+    private final MethodDatabase db;
+    private boolean check;
+    private final boolean aot;
+
+    public QuasarInstrumentor(boolean aot, ClassLoader classLoader, SuspendableClassifier classifier) {
+        this.db = new MethodDatabase(classLoader, classifier);
+        this.aot = aot;
+    }
+
+    public QuasarInstrumentor(ClassLoader classLoader, SuspendableClassifier classifier) {
+        this(false, classLoader, classifier);
+    }
+
+    public QuasarInstrumentor(boolean aot, ClassLoader classLoader) {
+        this(aot, classLoader, new DefaultSuspendableClassifier(classLoader));
+    }
+
+    public QuasarInstrumentor(ClassLoader classLoader) {
+        this(false, classLoader, new DefaultSuspendableClassifier(classLoader));
+    }
+
+    public boolean isAOT() {
+        return aot;
+    }
+
+    public boolean shouldInstrument(String className) {
+        if (className != null) {
+            className = className.replace('.', '/');
+            if (className.startsWith("co/paralleluniverse/fibers/instrument/") && !Debug.isUnitTest())
+                return false;
+            if (className.equals(Classes.FIBER_CLASS_NAME) || className.startsWith(Classes.FIBER_CLASS_NAME + '$'))
+                return false;
+            if (className.equals(Classes.STACK_NAME))
+                return false;
+            if (className.startsWith("org/objectweb/asm/"))
+                return false;
+            if (className.startsWith("org/netbeans/lib/"))
+                return false;
+            if (className.startsWith("java/lang/") || (!allowJdkInstrumentation && MethodDatabase.isJavaCore(className)))
+                return false;
+        }
+        return true;
+    }
+
+    public byte[] instrumentClass(String className, byte[] data) {
+        return shouldInstrument(className) ? instrumentClass(className, new ClassReader(data), false) : data;
+    }
+
+    public byte[] instrumentClass(String className, InputStream is) throws IOException {
+        return instrumentClass(className, new ClassReader(is), false);
+    }
+
+    byte[] instrumentClass(String className, InputStream is, boolean forceInstrumentation) throws IOException {
+        return instrumentClass(className, new ClassReader(is), forceInstrumentation);
+    }
+
+    private byte[] instrumentClass(String className, ClassReader r, boolean forceInstrumentation) {
+        className = className != null ? className.replace('.', '/') : null;
+
+        if (className != null) {
+            log(LogLevel.INFO, "TRANSFORM: %s %s", className,
+                    (db.getClassEntry(className) != null && db.getClassEntry(className).requiresInstrumentation()) ? "request" : "");
+        } else {
+            log(LogLevel.INFO, "TRANSFORM: null className");
+        }
+
+        final ClassWriter cw = new DBClassWriter(db, r);
+        final ClassVisitor cv = (check && EXAMINED_CLASS == null) ? new CheckClassAdapter(cw) : cw;
+
+        if (EXAMINED_CLASS != null && className != null && className.startsWith(EXAMINED_CLASS)) {
+            writeToFile(className.replace('/', '.') + "-before.class", getClassBuffer(r));
+            // cv = new TraceClassVisitor(cv, new PrintWriter(System.err));
+        }
+
+        final InstrumentClass ic = new InstrumentClass(cv, db, forceInstrumentation);
+        byte[] transformed;
+        try {
+            r.accept(ic, ClassReader.SKIP_FRAMES);
+            transformed = cw.toByteArray();
+        } catch (final Exception e) {
+            if (ic.hasSuspendableMethods()) {
+                error("Unable to instrument class " + className, e);
+                throw e;
+            } else {
+                if (className != null && !MethodDatabase.isProblematicClass(className))
+                    log(LogLevel.DEBUG, "Unable to instrument class " + className);
+                return null;
+            }
+        }
+
+        if (EXAMINED_CLASS != null) {
+            if (className != null && className.startsWith(EXAMINED_CLASS))
+                writeToFile(className.replace('/', '.') + "-after.class", transformed);
+
+            if (check) {
+                final ClassReader r2 = new ClassReader(transformed);
+                final ClassVisitor cv2 = new CheckClassAdapter(new TraceClassVisitor(null), true);
+                r2.accept(cv2, 0);
+            }
+        }
+
+        return transformed;
+    }
+
+    public MethodDatabase getMethodDatabase() {
+        return db;
+    }
+
+    public QuasarInstrumentor setCheck(boolean check) {
+        this.check = check;
+        return this;
+    }
+
+    public QuasarInstrumentor setAllowMonitors(boolean allowMonitors) {
+        db.setAllowMonitors(allowMonitors);
+        return this;
+    }
+
+    public QuasarInstrumentor setAllowBlocking(boolean allowBlocking) {
+        db.setAllowBlocking(allowBlocking);
+        return this;
+    }
+
+    public QuasarInstrumentor setLog(Log log) {
+        db.setLog(log);
+        return this;
+    }
+
+    public QuasarInstrumentor setVerbose(boolean verbose) {
+        db.setVerbose(verbose);
+        return this;
+    }
+
+    public QuasarInstrumentor setDebug(boolean debug) {
+        db.setDebug(debug);
+        return this;
+    }
+
+    public void log(LogLevel level, String msg, Object... args) {
+        db.log(level, msg, args);
+    }
+
+    public void error(String msg, Throwable ex) {
+        db.error(msg, ex);
+    }
+
+    public ArrayList<WorkListEntry> getWorkList() {
+        return db.getWorkList();
+    }
+
+    public void checkClass(File f) {
+        db.checkClass(f);
+    }
+
+    private static void writeToFile(String name, byte[] data) {
+        try (OutputStream os = Files.newOutputStream(Paths.get(name), StandardOpenOption.CREATE_NEW)) {
+            os.write(data);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] getClassBuffer(ClassReader r) {
+        try {
+            return (byte[]) ReflectionUtil.accessible(ClassReader.class.getDeclaredField("b")).get(r);
+        } catch (ReflectiveOperationException e) {
+            throw new AssertionError(e);
+        }
+    }
+}
---
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/SuspendableHelper.java
@@ -0,0 +1,133 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2013-2015, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.fibers.instrument;
+
+import co.paralleluniverse.common.util.ExtendedStackTraceElement;
+import co.paralleluniverse.common.util.Pair;
+import co.paralleluniverse.concurrent.util.MapUtil;
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.Instrumented;
+import co.paralleluniverse.fibers.Stack;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+// import java.lang.reflect.Executable;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ *
+ * @author pron
+ */
+public final class SuspendableHelper {
+    static boolean javaAgent;
+    static final Set<Pair<String, String>> waivers = Collections.newSetFromMap(MapUtil.<Pair<String, String>, Boolean>newConcurrentHashMap());
+
+    public static boolean isJavaAgentActive() {
+        return javaAgent;
+    }
+
+    public static boolean isInstrumented(Class clazz) {
+        return clazz != null && clazz.isAnnotationPresent(Instrumented.class);
+    }
+
+    public static /*Executable*/ Member lookupMethod(ExtendedStackTraceElement ste) {
+        if (ste.getDeclaringClass() == null)
+            return null;
+
+        if (ste.getMethod() != null)
+            return ste.getMethod();
+
+        for (Method m : ste.getDeclaringClass().getDeclaredMethods()) {
+            if (m.getName().equals(ste.getMethodName())) {
+                final Instrumented i = getAnnotation(m, Instrumented.class);
+                if (m.isSynthetic() || isWaiver(m.getDeclaringClass().getName(), m.getName()) || i != null && ste.getLineNumber() >= i.methodStart() && ste.getLineNumber() <= i.methodEnd())
+                    return m;
+            }
+        }
+        return null;
+    }
+
+    public static Pair<Boolean, int[]> isCallSiteInstrumented(/*Executable*/ Member m, int sourceLine, ExtendedStackTraceElement[] stes, int currentSteIdx) {
+        if (m == null)
+            return new Pair<>(false, null);
+
+        if (isSyntheticAndNotLambda(m))
+            return new Pair<>(true, null);
+
+        ExtendedStackTraceElement ste = stes[currentSteIdx];
+        if (currentSteIdx - 1 >= 0
+                // `verifySuspend` and `popMethod` calls are not suspendable call sites, not verifying them.
+                && ((stes[currentSteIdx - 1].getClassName().equals(Fiber.class.getName()) && stes[currentSteIdx - 1].getMethodName().equals("verifySuspend"))
+                || (stes[currentSteIdx - 1].getClassName().equals(Stack.class.getName()) && stes[currentSteIdx - 1].getMethodName().equals("popMethod")))) {
+            return new Pair<>(true, null);
+        } else {
+            Instrumented i = getAnnotation(m, Instrumented.class);
+            if (i != null) {
+                final int[] scs = i.suspendableCallSites();
+                for (int j : scs) {
+                    if (j == sourceLine)
+                        return new Pair<>(true, scs);
+                }
+                return new Pair<>(false, scs);
+            }
+            return new Pair<>(false, null);
+        }
+    }
+
+    public static boolean isInstrumented(Member m) {
+        return m != null && (isSyntheticAndNotLambda(m) || getAnnotation(m, Instrumented.class) != null);
+    }
+    
+    public static boolean isSyntheticAndNotLambda(Member m) {
+        return m.isSynthetic() && !m.getName().startsWith(Classes.LAMBDA_METHOD_PREFIX);
+    }
+
+    public static boolean isOptimized(Member m) {
+        if (m == null)
+            return false;
+
+        final Instrumented i = getAnnotation(m, Instrumented.class);
+        return (i != null && i.methodOptimized());
+    }
+
+    private static <T extends Annotation> T getAnnotation(Member m, Class<T> annotationClass) {
+        if (m == null || annotationClass == null)
+            return  null;
+
+        if (m instanceof Constructor)
+            return ((Constructor<?>)m).getAnnotation(annotationClass);
+        else
+            return ((Method)m).getAnnotation(annotationClass);
+    }
+
+    public static void addWaiver(String className, String methodName) {
+        waivers.add(new Pair<>(className, methodName));
+    }
+
+    public static boolean isWaiver(String className, String methodName) {
+        if (className.startsWith("java.lang.reflect")
+                || className.startsWith("sun.reflect")
+                || className.startsWith("com.sun.proxy")
+                || className.contains("$ByteBuddy$")
+                || (className.equals("co.paralleluniverse.strands.SuspendableUtils$VoidSuspendableCallable") && methodName.equals("run"))
+                || (className.equals("co.paralleluniverse.strands.dataflow.Var") && methodName.equals("set")))
+            return true;
+        return waivers.contains(new Pair<>(className, methodName));
+    }
+
+    private SuspendableHelper() {
+    }
+}
