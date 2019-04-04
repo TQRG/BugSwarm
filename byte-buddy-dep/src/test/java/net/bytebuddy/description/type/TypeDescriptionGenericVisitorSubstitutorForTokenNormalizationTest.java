d0df7b0f527d4cc1a791e970154954e3ff81be65
tdurieux <durieuxthomas@gmail.com>
2019-04-04 23:44:20 +0000

    Buggy version of raphw-byte-buddy-102249353

---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/description/type/TypeDescriptionGenericVisitorSubstitutorForTokenNormalizationTest.java
@@ -0,0 +1,66 @@
+package net.bytebuddy.description.type;
+
+import net.bytebuddy.description.annotation.AnnotationDescription;
+import net.bytebuddy.description.annotation.AnnotationList;
+import net.bytebuddy.dynamic.TargetType;
+import net.bytebuddy.test.utility.MockitoRule;
+import net.bytebuddy.test.utility.ObjectPropertyAssertion;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+public class TypeDescriptionGenericVisitorSubstitutorForTokenNormalizationTest {
+
+    private static final String FOO = "foo";
+
+    @Rule
+    public TestRule mockitoRule = new MockitoRule(this);
+
+    @Mock
+    private TypeDescription.Generic source, target;
+
+    @Mock
+    private AnnotationDescription annotationDescription;
+
+    @Before
+    public void setUp() throws Exception {
+        when(source.getSymbol()).thenReturn(FOO);
+        when(source.getDeclaredAnnotations()).thenReturn(new AnnotationList.Explicit(annotationDescription));
+    }
+
+    @Test
+    public void testTargetType() throws Exception {
+        assertThat(new TypeDescription.Generic.Visitor.Substitutor.ForTokenNormalization(target).onSimpleType(TargetType.GENERIC_DESCRIPTION), is(target));
+    }
+
+    @Test
+    public void testNotTargetType() throws Exception {
+        assertThat(new TypeDescription.Generic.Visitor.Substitutor.ForTokenNormalization(target).onSimpleType(source), is(source));
+    }
+
+    @Test
+    public void testTypeVariable() throws Exception {
+        assertThat(new TypeDescription.Generic.Visitor.Substitutor.ForTokenNormalization(target).onTypeVariable(source),
+                is((TypeDescription.Generic) new TypeDescription.Generic.OfTypeVariable.Symbolic(FOO, Collections.singletonList(annotationDescription))));
+    }
+
+    @Test
+    public void testObjectProperties() throws Exception {
+        ObjectPropertyAssertion.of(TypeDescription.Generic.Visitor.Substitutor.ForTokenNormalization.class)
+                .refine(new ObjectPropertyAssertion.Refinement<TypeDescription>() {
+                    @Override
+                    public void apply(TypeDescription mock) {
+                        when(mock.asGenericType()).thenReturn(Mockito.mock(TypeDescription.Generic.class));
+                    }
+                }).apply();
+    }
+}
