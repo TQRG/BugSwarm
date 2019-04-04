e8ee770b2592d657334e8636bb7e1c45ed3ae216
tdurieux <durieuxthomas@gmail.com>
2019-04-04 23:11:21 +0000

    Buggy version of raphw-byte-buddy-96233717

---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/description/method/MethodListEmptyTest.java
@@ -0,0 +1,25 @@
+package net.bytebuddy.description.method;
+
+import org.junit.Test;
+
+import static net.bytebuddy.matcher.ElementMatchers.none;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MethodListEmptyTest {
+
+    @Test
+    public void testTokenListWithFilter() throws Exception {
+        assertThat(new MethodList.Empty().asTokenList(none()).size(), is(0));
+    }
+
+    @Test
+    public void testTokenListWithoutFilter() throws Exception {
+        assertThat(new MethodList.Empty().asTokenList().size(), is(0));
+    }
+
+    @Test
+    public void testDeclaredList() throws Exception {
+        assertThat(new MethodList.Empty().asDefined().size(), is(0));
+    }
+}
