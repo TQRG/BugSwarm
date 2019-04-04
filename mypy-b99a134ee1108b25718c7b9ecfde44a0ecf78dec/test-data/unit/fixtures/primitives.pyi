9ff4f3a7107084280d05093ec0c007da382e7326
tdurieux <durieuxthomas@gmail.com>
2019-04-04 19:19:06 +0000

    Buggy version of python-mypy-228813888

---
+++ mypy-b99a134ee1108b25718c7b9ecfde44a0ecf78dec/test-data/unit/fixtures/primitives.pyi
@@ -0,0 +1,20 @@
+# builtins stub with non-generic primitive types
+
+class object:
+    def __init__(self) -> None: pass
+    def __str__(self) -> str: pass
+
+class type:
+    def __init__(self, x) -> None: pass
+
+class int:
+    def __add__(self, i: int) -> int: pass
+class float: pass
+class complex: pass
+class bool: pass
+class str:
+    def __add__(self, s: str) -> str: pass
+class bytes: pass
+class bytearray: pass
+class tuple: pass
+class function: pass
