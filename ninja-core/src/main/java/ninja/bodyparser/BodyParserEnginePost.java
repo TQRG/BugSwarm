89a03318bd3b544868ba59373b59dcb8055fd7f4
tdurieux <durieuxthomas@gmail.com>
2019-04-04 20:33:36 +0000

    Buggy version of ninjaframework-ninja-108917105

---
+++ ninja-core/src/main/java/ninja/bodyparser/BodyParserEnginePost.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (C) 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ninja.bodyparser;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import ninja.ContentTypes;
+import ninja.Context;
+import ninja.params.ParamParser;
+import ninja.params.ParamParsers;
+import ninja.params.ParamParsers.ArrayParamParser;
+import ninja.params.ParamParsers.ListParamParser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Sets;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+
+@Singleton
+public class BodyParserEnginePost implements BodyParserEngine {
+
+    private final Logger logger = LoggerFactory.getLogger(BodyParserEnginePost.class);
+
+    private final ParamParsers paramParsers;
+
+    @Inject
+    public BodyParserEnginePost(ParamParsers paramParsers) {
+        this.paramParsers = paramParsers;
+    }
+
+    @Override
+    public <T> T invoke(Context context, Class<T> classOfT) {
+
+        T t = null;
+
+        try {
+            t = classOfT.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            logger.error("Can't create new instance of class {}", classOfT.getName(), e);
+            return null;
+        }
+
+        Set<String> declaredFields = getAllDeclaredFieldsAsStringSet(classOfT);
+
+        for (Entry<String, String[]> ent : context.getParameters().entrySet()) {
+
+            if (declaredFields.contains(ent.getKey())) {
+
+                try {
+
+                    Field field = classOfT.getDeclaredField(ent.getKey());
+                    Class<?> fieldType = field.getType();
+                    field.setAccessible(true);
+
+                    String[] values = ent.getValue();
+
+                    if (Collection.class.isAssignableFrom(fieldType) || List.class.isAssignableFrom(fieldType)) {
+
+                        ListParamParser<?> parser = (ListParamParser<?>) paramParsers.getListParser(getGenericType(field));
+                        if (parser == null) {
+                            logger.warn("No parser defined for a collection of type {}", getGenericType(field).getCanonicalName());
+                        } else {
+                            field.set(t, parser.parseParameter(field.getName(), values, context.getValidation()));
+                        }
+
+                    } else if (fieldType.isArray()) {
+
+                        ArrayParamParser<?> parser = paramParsers.getArrayParser(fieldType);
+                        if (parser == null) {
+                            logger.warn("No parser defined for an array of type {}", fieldType.getComponentType().getCanonicalName());
+                        } else {
+                            field.set(t, parser.parseParameter(field.getName(), values, context.getValidation()));
+                        }
+
+                    } else {
+
+                        ParamParser<?> parser = (ParamParser<?>) paramParsers.getParamParser(fieldType);
+                        if (parser == null) {
+                            logger.warn("No parser defined for type {}", fieldType.getCanonicalName());
+                        } else {
+                            field.set(t, parser.parseParameter(field.getName(), values[0], context.getValidation()));
+                        }
+
+                    }
+
+                } catch (NoSuchFieldException 
+                        | SecurityException 
+                        | IllegalArgumentException 
+                        | IllegalAccessException e) {
+
+                    logger.warn(
+                            "Error parsing incoming Post request into class {}. Key {} and value {}.", 
+                            classOfT.getName(), ent.getKey(), ent.getValue(), e);
+                }
+
+            }
+
+        }
+        return t;
+    }
+
+    public String getContentType() {
+        return ContentTypes.APPLICATION_POST_FORM;
+    }
+
+    private <T> Set<String> getAllDeclaredFieldsAsStringSet(Class<T> clazz) {
+
+        Set<String> declaredFields = Sets.newHashSet();
+
+        for (Field field : clazz.getDeclaredFields()) {
+            declaredFields.add(field.getName());
+        }
+
+        return declaredFields;
+
+    }
+
+    private Class<?> getGenericType(Field field) {
+        ParameterizedType genericType = (ParameterizedType) field.getGenericType();
+        return (Class<?>) genericType.getActualTypeArguments()[0];
+    }
+}
---
+++ ninja-core/src/main/java/ninja/validation/ValidationImpl.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (C) 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ninja.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Validation object
+ *
+ * @author James Roper
+ * @author Philip Sommer
+ */
+public class ValidationImpl implements Validation {
+
+    private final List<FieldViolation> fieldViolations = new ArrayList<FieldViolation>();
+    private final List<ConstraintViolation> generalViolations = new ArrayList<ConstraintViolation>();
+    private final List<FieldViolation> beanViolations = new ArrayList<FieldViolation>();
+
+    @Override
+    public boolean hasViolations() {
+        return !this.fieldViolations.isEmpty() || !this.generalViolations.isEmpty()
+                || !this.beanViolations.isEmpty();
+    }
+
+    @Override
+    public void addFieldViolation(FieldViolation fieldViolation) {
+        if (fieldViolation.field == null) {
+            this.generalViolations.add(fieldViolation.constraintViolation);
+        } else {
+            this.fieldViolations.add(fieldViolation);
+        }
+    }
+
+    @Override
+    public void addFieldViolation(String field, ConstraintViolation constraintViolation) {
+        addFieldViolation(new FieldViolation(field, constraintViolation));
+    }
+
+    @Override
+    public boolean hasFieldViolation(String field) {
+        for (FieldViolation fieldViolation : this.fieldViolations) {
+            if (fieldViolation.field.contentEquals(field)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public List<FieldViolation> getFieldViolations() {
+        return this.fieldViolations;
+    }
+
+    @Override
+    public List<FieldViolation> getFieldViolations(String field) {
+        List<FieldViolation> violationsForThisField = new ArrayList<FieldViolation>();
+        for (FieldViolation fieldViolation : this.fieldViolations) {
+            if (fieldViolation.field.contentEquals(field)) {
+                violationsForThisField.add(fieldViolation);
+            }
+        }
+        return violationsForThisField;
+    }
+
+    @Override
+    public void addBeanViolation(FieldViolation fieldViolation) {
+        this.beanViolations.add(fieldViolation);
+    }
+
+    @Override
+    public boolean hasBeanViolation(String field) {
+        for (FieldViolation beanViolation : this.beanViolations) {
+            if (beanViolation.field.contentEquals(field)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasBeanViolations() {
+        return !this.beanViolations.isEmpty();
+    }
+
+    @Override
+    public List<FieldViolation> getBeanViolations() {
+        return this.beanViolations;
+    }
+
+    @Override
+    public void addViolation(ConstraintViolation constraintViolation) {
+        this.generalViolations.add(constraintViolation);
+    }
+
+    @Override
+    public List<ConstraintViolation> getGeneralViolations() {
+        return this.generalViolations;
+    }
+}
