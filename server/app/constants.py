4c7bd9d3f762310e0031981649f9cc82834707e0
tdurieux <durieuxthomas@gmail.com>
2019-04-04 17:43:34 +0000

    Buggy version of Cal-CS-61A-Staff-ok-73050629

---
+++ server/app/constants.py
@@ -0,0 +1,8 @@
+"""App constants"""
+
+API_PREFIX = '/api'
+STUDENT_ROLE = 'student'
+STAFF_ROLE = 'staff'
+VALID_ROLES = [STUDENT_ROLE, STAFF_ROLE]
+GRADES_BUCKET = 'ok_grades_bucket'
+TIMEZONE = 'America/Los_Angeles'
