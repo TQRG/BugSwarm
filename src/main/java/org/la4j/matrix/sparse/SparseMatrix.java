14f40000d676f3a9a92424932f0d16acbc222729
tdurieux <durieuxthomas@gmail.com>
2019-04-05 00:33:43 +0000

    Buggy version of vkostyukov-la4j-44960461

---
+++ src/main/java/org/la4j/matrix/sparse/SparseMatrix.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2011-2014, by Vladimir Kostyukov and Contributors.
+ * 
+ * This file is part of la4j project (http://la4j.org)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributor(s): -
+ * 
+ */
+
+package org.la4j.matrix.sparse;
+
+import org.la4j.matrix.Matrix;
+import org.la4j.matrix.functor.MatrixAccumulator;
+import org.la4j.matrix.functor.MatrixProcedure;
+import org.la4j.vector.Vector;
+import org.la4j.vector.functor.VectorAccumulator;
+import org.la4j.vector.functor.VectorFunction;
+import org.la4j.vector.functor.VectorProcedure;
+
+public interface SparseMatrix extends Matrix {
+
+    /**
+     * Returns the cardinality (the number of non-zero elements)
+     * of this sparse matrix.
+     * 
+     * @return the cardinality of this matrix
+     */
+    int cardinality();
+
+    /**
+     * Returns the density (non-zero elements divided by total elements)
+     * of this sparse matrix.
+     * 
+     * @return the density of this matrix
+     */
+    double density();
+
+    /**
+     * Whether or not the specified element is zero.
+     *
+     * @param i element's row index
+     * @param j element's column index
+     *
+     * @return {@code true} if specified element is zero, {@code false} otherwise
+     */
+    boolean isZeroAt(int i, int j);
+
+    /**
+     * Whether or not the specified element is not zero.
+     *
+     * @param i element's row index
+     * @param j element's column index
+     *
+     * @return {@code true} if specified element is not zero, {@code false} otherwise
+     */
+    boolean nonZeroAt(int i, int j);
+
+    /**
+     * Applies given {@code procedure} to each non-zero element of this matrix.
+     *
+     * @param procedure the matrix procedure
+     */
+    void eachNonZero(MatrixProcedure procedure);
+
+    /**
+     * Deprecated. Use {@link #eachNonZeroInRow(int, VectorFunction)} instead.
+     * <p/>
+     * Applies given {@code procedure} to each non-zero element of specified row of this matrix.
+     *
+     * @param i the row index
+     * @param procedure the matrix procedure
+     */
+    @Deprecated
+    void eachNonZeroInRow(int i, MatrixProcedure procedure);
+    
+    /**
+     * Applies the given {@code procedure} to each non-zero element of the specified row of this matrix.
+     * 
+     * @param i the row index. 
+     * @param procedure the {@link VectorProcedure}. 
+     */
+    void eachNonZeroInRow(int i, VectorProcedure procedure);
+
+    /**
+     * Deprecated. Use {@link #eachNonZeroInColumn(int, VectorFunction)} instead.
+     * <p/>
+     * Applies given {@code procedure} to each non-zero element of specified column of this matrix.
+     *
+     * @param j the column index
+     * @param procedure the matrix procedure
+     */
+    @Deprecated
+    void eachNonZeroInColumn(int j, MatrixProcedure procedure);
+    
+    /**
+     * Applies the given {@code procedure} to each non-zero element of the specified column of this matrix.
+     * 
+     * @param j the column index.
+     * @param procedure the {@link VectorProcedure}.
+     */
+    void eachNonZeroInColumn(int j, VectorProcedure procedure);
+
+    /**
+     * Folds non-zero elements of this matrix with given {@code accumulator}.
+     *
+     * @param accumulator the matrix accumulator
+     *
+     * @return the accumulated value
+     */
+    double foldNonZero(MatrixAccumulator accumulator);
+
+    /**
+     * Deprecated. Use {@link #foldNonZeroInRow(int, VectorFunction)} instead.
+     * <p/>
+     * Folds non-zero elements of specified row in this matrix with given {@code accumulator}.
+     *
+     * @param i the row index
+     * @param accumulator the matrix accumulator
+     *
+     * @return the accumulated value
+     */
+    @Deprecated
+    double foldNonZeroInRow(int i, MatrixAccumulator accumulator);
+    
+    /**
+     * Folds non-zero elements of the specified row in this matrix with the given {@code accumulator}.
+     * 
+     * @param i the row index.
+     * @param accumulator the {@link VectorAccumulator}.
+     * 
+     * @return the accumulated value.
+     */
+    double foldNonZeroInRow(int i, VectorAccumulator accumulator);
+
+    /**
+     * Deprecated. Use {@link #foldNonZeroInColumn(int, VectorFunction)} instead.
+     * <p/>
+     * Folds non-zero elements of specified column in this matrix with given {@code accumulator}.
+     *
+     * @param j the column index
+     * @param accumulator the matrix accumulator
+     *
+     * @return the accumulated value
+     */
+    @Deprecated
+    double foldNonZeroInColumn(int j, MatrixAccumulator accumulator);
+    
+    /**
+     * Folds non-zero elements of the specified column in this matrix with the given {@code accumulator}.
+     * 
+     * @param j the column index.
+     * @param accumulator the {@link VectorAccumulator}.
+     * 
+     * @return the accumulated value.
+     */
+    double foldNonZeroInColumn(int j, VectorAccumulator accumulator);
+
+    /**
+     * Deprecated. Use {@link #foldNonZeroInColumns(VectorAccumulator)} instead.
+     * <p/>
+     * Folds non-zero elements (in a column-by-column manner) of this matrix with given {@code accumulator}.
+     *
+     * @param accumulator the matrix accumulator
+     *
+     * @return the accumulated vector
+     */
+    @Deprecated
+    Vector foldNonZeroInColumns(MatrixAccumulator accumulator);
+
+    /**
+     * Folds non-zero elements (in a column-by-column manner) of this matrix with given {@code accumulator}.
+     *
+     * @param accumulator the matrix accumulator
+     *
+     * @return the accumulated vector
+     */
+    Vector foldNonZeroInColumns(VectorAccumulator accumulator);
+
+    /**
+     * Deprecated. use {@link #foldNonZeroInRows(VectorAccumulator)} instead.
+     * <p/>
+     * Folds non-zero elements (in a row-by-row manner) of this matrix with given {@code accumulator}.
+     *
+     * @param accumulator the matrix accumulator
+     *
+     * @return the accumulated vector
+     */
+    @Deprecated
+    Vector foldNonZeroInRows(MatrixAccumulator accumulator);
+
+    /**
+     * Folds non-zero elements (in a row-by-row manner) of this matrix with given {@code accumulator}.
+     *
+     * @param accumulator the matrix accumulator
+     *
+     * @return the accumulated vector
+     */
+    Vector foldNonZeroInRows(VectorAccumulator accumulator);
+
+    /**
+     * Gets the specified element, or a {@code defaultValue} if there
+     * is no actual element at ({@code i}, {@code j}) in this sparse matrix.
+     *
+     * @param i the element's row index
+     * @param j the element's column index
+     * @param defaultValue the default value
+     *
+     * @return the element of this vector or a default value
+     */
+    double getOrElse(int i, int j, double defaultValue);
+}
---
+++ src/main/java/org/la4j/vector/dense/DenseVector.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2011-2014, by Vladimir Kostyukov and Contributors.
+ * 
+ * This file is part of la4j project (http://la4j.org)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributor(s): -
+ * 
+ */
+
+package org.la4j.vector.dense;
+
+import org.la4j.LinearAlgebra;
+import org.la4j.matrix.Matrix;
+import org.la4j.matrix.dense.Basic2DMatrix;
+import org.la4j.vector.AbstractVector;
+import org.la4j.vector.Vector;
+import org.la4j.vector.operation.VectorOperation;
+import org.la4j.vector.operation.VectorVectorOperation;
+
+import java.util.Random;
+
+/**
+ * A dense vector.
+ * 
+ * A vector represents an array of elements. It can be re-sized.
+ * 
+ * A dense data structure usually stores data in an underlying array. Zero elements
+ * take up memory space. If you want a data structure that will not have zero
+ * elements take up memory space, try a sparse structure.
+ * 
+ * However, fetch/store operations on dense data structures only take O(1) time,
+ * instead of the O(log n) time on sparse structures.
+ * 
+ */
+public abstract class DenseVector extends AbstractVector {
+
+    /**
+     * Creates a zero {@link DenseVector} of the given {@code length}.
+     */
+    public static DenseVector zero(int length) {
+        return BasicVector.zero(length);
+    }
+
+    /**
+     * Creates an unit {@link DenseVector} of the given {@code length}.
+     */
+    public static DenseVector unit(int length) {
+        return DenseVector.constant(length, 1.0);
+    }
+
+    /**
+     * Creates a new {@link BasicVector} from the given {@code values}.
+     */
+    public static DenseVector of(double... values) {
+        return BasicVector.of(values);
+    }
+
+    /**
+     * Creates a new {@link DenseVector} from the given {@code array}.
+     */
+    public static DenseVector fromArray(double[] array) {
+        return BasicVector.fromArray(array);
+    }
+
+    /**
+     * Creates a constant {@link DenseVector} of the given {@code length} with
+     * the given {@code value}.
+     */
+    public static DenseVector constant(int length, double value) {
+        return BasicVector.constant(length, value);
+    }
+
+    /**
+     * Creates a random {@link DenseVector} of the given {@code length} with
+     * the given {@code Random}.
+     */
+    public static DenseVector random(int length, Random random) {
+        return BasicVector.random(length, random);
+    }
+
+    public DenseVector(int length) {
+        super(LinearAlgebra.DENSE_FACTORY, length);
+    }
+
+    @Override
+    public <T> T apply(VectorOperation<T> operation) {
+        return operation.apply(this);
+    }
+
+    @Override
+    public <T> T apply(VectorVectorOperation<T> operation, Vector that) {
+        return that.apply(operation.curry(this));
+    }
+
+    /**
+     * Converts this dense vector to a double array.
+     *
+     * @return an array representation of this vector
+     */
+    public abstract double[] toArray();
+
+    @Override
+    public Matrix toRowMatrix() {
+        // TODO: use DenseMatrix.ofShape()
+        Matrix result = new Basic2DMatrix(1, length);
+        result.setRow(0, this);
+        return result;
+    }
+
+    @Override
+    public Matrix toColumnMatrix() {
+        // TODO: use SparseMatrix.ofShape()
+        Matrix result = new Basic2DMatrix(length, 1);
+        result.setColumn(0, this);
+        return result;
+    }
+}
