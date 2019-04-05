a011cd070553bf9d626fb1ff59585baa8fe1bbbd
tdurieux <durieuxthomas@gmail.com>
2019-04-05 12:16:17 +0000

    Buggy version of wmixvideo-nfe-375999357

---
+++ src/main/java/com/fincatto/documentofiscal/transformers/DFDateTimeTransformer.java
@@ -0,0 +1,22 @@
+package com.fincatto.documentofiscal.transformers;
+
+import org.simpleframework.xml.transform.Transform;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DFDateTimeTransformer implements Transform<ZonedDateTime> {
+
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+
+    @Override
+    public ZonedDateTime read(final String data) throws Exception {
+        return ZonedDateTime.parse(data, format.withZone(ZoneId.systemDefault()));
+    }
+
+    @Override
+    public String write(final ZonedDateTime data) throws Exception {
+        return format.format(data);
+    }
+}
