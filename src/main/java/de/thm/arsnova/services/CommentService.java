dd37f317344412cd785f5902cc45e33c449fb7aa
tdurieux <durieuxthomas@gmail.com>
2019-04-05 16:36:40 +0000

    Buggy version of thm-projects-arsnova-backend-347860322

---
+++ src/main/java/de/thm/arsnova/services/CommentService.java
@@ -0,0 +1,23 @@
+package de.thm.arsnova.services;
+
+import de.thm.arsnova.entities.Comment;
+import de.thm.arsnova.entities.migration.v2.CommentReadingCount;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface CommentService extends EntityService<Comment> {
+	boolean save(Comment comment);
+
+	int count(String roomId);
+
+	CommentReadingCount countRead(String roomId, String username);
+
+	List<Comment> getByRoomId(String roomId, int offset, int limit);
+
+	Comment getAndMarkRead(String commentId) throws IOException;
+
+	void delete(String commentId);
+
+	void deleteByRoomId(String roomId);
+}
