45cfc357ac5a1509a794059e749f4dd2c4f3e256
tdurieux <durieuxthomas@gmail.com>
2019-04-05 00:23:18 +0000

    Buggy version of lviggiano-owner-137628142

---
+++ owner-extras/src/test/java/org/aeonbits/owner/converters/ByteSizeConverterTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2012-2015, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
+package org.aeonbits.owner.converters;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.ConfigFactory;
+import org.aeonbits.owner.util.bytesize.ByteSize;
+import org.aeonbits.owner.util.bytesize.ByteSizeUnit;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Stefan Freyr Stefansson
+ */
+public class ByteSizeConverterTest {
+    public interface ByteSizeConfig extends Config {
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10 byte")
+        ByteSize singular10byteWithSpace();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10byte")
+        ByteSize singular10byteWithoutSpace();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10 bytes")
+        ByteSize plural10byte();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10m")
+        ByteSize short10mebibytes();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10mi")
+        ByteSize medium10mebibytes();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10mib")
+        ByteSize long10mebibytes();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10 megabytes")
+        ByteSize full10megabytes();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("10 sillybyte")
+        ByteSize invalidSillybyte();
+
+        @ConverterClass(ByteSizeConverter.class)
+        @DefaultValue("megabyte")
+        ByteSize invalidNoNumber();
+    }
+
+    @Test
+    public void testValidByteSizeConverter(){
+        ByteSizeConfig cfg = ConfigFactory.create(ByteSizeConfig.class);
+        ByteSize bs;
+
+        bs = cfg.plural10byte();
+        Assert.assertEquals(bs, new ByteSize(10, ByteSizeUnit.BYTES));
+
+        bs = cfg.singular10byteWithoutSpace();
+        Assert.assertEquals(bs, new ByteSize(10, ByteSizeUnit.BYTES));
+
+        bs = cfg.singular10byteWithSpace();
+        Assert.assertEquals(bs, new ByteSize(10, ByteSizeUnit.BYTES));
+
+        ByteSize compare = new ByteSize(10, ByteSizeUnit.MEBIBYTES);
+        Assert.assertEquals(compare, cfg.short10mebibytes());
+        Assert.assertEquals(compare, cfg.medium10mebibytes());
+        Assert.assertEquals(compare, cfg.long10mebibytes());
+        Assert.assertNotEquals(compare, cfg.full10megabytes());
+        Assert.assertEquals(new ByteSize(10, ByteSizeUnit.MEGABYTES), cfg.full10megabytes());
+    }
+
+    @Test
+    public void testInvalid() throws NoSuchMethodException, IllegalAccessException {
+        ByteSizeConfig cfg = ConfigFactory.create(ByteSizeConfig.class);
+        ByteSize bs;
+        for (String method : new String[]{"invalidSillybyte", "invalidNoNumber"}) {
+            Method m = ByteSizeConfig.class.getDeclaredMethod(method);
+            try {
+                bs = (ByteSize) m.invoke(cfg);
+                fail(String.format("Invalid byte size [%s] should have thrown an exception. Instead we parsed: %s", method, bs));
+            } catch (InvocationTargetException e) {
+                if (!(e.getCause() instanceof IllegalArgumentException)){
+                    fail("Got an unexpected exception type when calling method: " + method);
+                }
+            }
+        }
+    }
+
+}
