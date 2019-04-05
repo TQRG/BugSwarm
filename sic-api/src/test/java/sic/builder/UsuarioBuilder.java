84a6b8ea69ecc1f56a909fb5278b93296d3d40c3
tdurieux <durieuxthomas@gmail.com>
2019-04-05 04:22:14 +0000

    Buggy version of belluccifranco-sic-211965193

---
+++ sic-api/src/test/java/sic/builder/UsuarioBuilder.java
@@ -0,0 +1,47 @@
+package sic.builder;
+
+import sic.modelo.Usuario;
+
+public class UsuarioBuilder {
+    
+    private long id_Usuario = 0L;
+    private String nombre = "Daenerys Targaryen";
+    private String password = "LaQueNoArde";
+    private String token = "yJhbGci1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJjZWNpbGlvIn0.MCfaorSC7Wdc8rSW7BJizasfzsm";
+    private boolean permisosAdministrador = true;
+    private boolean eliminado = false;
+    
+    public Usuario build() {
+        return new Usuario(id_Usuario, nombre, password, token, permisosAdministrador, eliminado);
+    }
+    
+    public UsuarioBuilder withId_Usuario(long idUsuario) {
+        this.id_Usuario = idUsuario;
+        return this;
+    }
+    
+    public UsuarioBuilder withNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+    
+    public UsuarioBuilder withPassword(String password) {
+        this.password = password;
+        return this;
+    }
+    
+    public UsuarioBuilder withToken(String token) {
+        this.token = token;
+        return this;
+    }
+    
+    public UsuarioBuilder withPermisosAdministrador(boolean permisosAdministrador) {
+        this.permisosAdministrador = permisosAdministrador;
+        return this;
+    }
+    
+    public UsuarioBuilder withEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+        return this;
+    }
+}
