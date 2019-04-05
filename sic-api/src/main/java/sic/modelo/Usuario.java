dff66662f5cdc86e9b22d0c436ff43f98658d5d9
tdurieux <durieuxthomas@gmail.com>
2019-04-05 04:31:36 +0000

    Buggy version of belluccifranco-sic-212962404

---
+++ sic-api/src/main/java/sic/modelo/Usuario.java
@@ -0,0 +1,45 @@
+package sic.modelo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"nombre"})
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long id_Usuario;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    
+    @Column(nullable = false)
+    private String token;
+
+    private boolean permisosAdministrador;
+
+    private boolean eliminado;
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
