a0707c76ec5209764d3e8514b6f4c9f2e73525e9
tdurieux <durieuxthomas@gmail.com>
2019-04-05 23:04:17 +0000

    Buggy version of python-mypy-324391447

---
+++ mypy/server/aststrip.py
@@ -0,0 +1,193 @@
+"""Strip AST from semantic information.
+
+This is used in fine-grained incremental checking to reprocess existing AST nodes.
+"""
+
+import contextlib
+from typing import Union, Iterator, Optional
+
+from mypy.nodes import (
+    Node, FuncDef, NameExpr, MemberExpr, RefExpr, MypyFile, FuncItem, ClassDef, AssignmentStmt,
+    ImportFrom, Import, TypeInfo, SymbolTable, Var, CallExpr, Decorator, OverloadedFuncDef,
+    UNBOUND_IMPORTED, GDEF
+)
+from mypy.traverser import TraverserVisitor
+
+
+def strip_target(node: Union[MypyFile, FuncItem, OverloadedFuncDef]) -> None:
+    """Strip a fine-grained incremental mode target from semantic information."""
+    visitor = NodeStripVisitor()
+    if isinstance(node, MypyFile):
+        visitor.strip_file_top_level(node)
+    else:
+        node.accept(visitor)
+
+
+class NodeStripVisitor(TraverserVisitor):
+    def __init__(self) -> None:
+        self.type = None  # type: Optional[TypeInfo]
+        self.names = None  # type: Optional[SymbolTable]
+        self.is_class_body = False
+        # By default, process function definitions. If False, don't -- this is used for
+        # processing module top levels.
+        self.recurse_into_functions = True
+
+    def strip_file_top_level(self, file_node: MypyFile) -> None:
+        """Strip a module top-level (don't recursive into functions)."""
+        self.names = file_node.names
+        self.recurse_into_functions = False
+        file_node.accept(self)
+
+    def visit_class_def(self, node: ClassDef) -> None:
+        """Strip class body and type info, but don't strip methods."""
+        node.info.type_vars = []
+        node.info.bases = []
+        node.info.abstract_attributes = []
+        node.info.mro = []
+        node.info.add_type_vars()
+        node.base_type_exprs.extend(node.removed_base_type_exprs)
+        node.removed_base_type_exprs = []
+        with self.enter_class(node.info):
+            super().visit_class_def(node)
+
+    def visit_func_def(self, node: FuncDef) -> None:
+        if not self.recurse_into_functions:
+            return
+        node.expanded = []
+        node.type = node.unanalyzed_type
+        with self.enter_method(node.info) if node.info else nothing():
+            super().visit_func_def(node)
+
+    def visit_decorator(self, node: Decorator) -> None:
+        node.var.type = None
+        for expr in node.decorators:
+            expr.accept(self)
+        if self.recurse_into_functions:
+            node.func.accept(self)
+
+    def visit_overloaded_func_def(self, node: OverloadedFuncDef) -> None:
+        if not self.recurse_into_functions:
+            return
+        if node.impl:
+            # Revert change made during semantic analysis pass 2.
+            assert node.items[-1] is not node.impl
+            node.items.append(node.impl)
+        super().visit_overloaded_func_def(node)
+
+    @contextlib.contextmanager
+    def enter_class(self, info: TypeInfo) -> Iterator[None]:
+        # TODO: Update and restore self.names
+        old_type = self.type
+        old_is_class_body = self.is_class_body
+        self.type = info
+        self.is_class_body = True
+        yield
+        self.type = old_type
+        self.is_class_body = old_is_class_body
+
+    @contextlib.contextmanager
+    def enter_method(self, info: TypeInfo) -> Iterator[None]:
+        # TODO: Update and restore self.names
+        old_type = self.type
+        old_is_class_body = self.is_class_body
+        self.type = info
+        self.is_class_body = False
+        yield
+        self.type = old_type
+        self.is_class_body = old_is_class_body
+
+    def visit_assignment_stmt(self, node: AssignmentStmt) -> None:
+        node.type = node.unanalyzed_type
+        if node.type and self.is_class_body:
+            # Remove attribute defined in the class body from the class namespace to avoid
+            # bogus "Name already defined" errors.
+            #
+            # TODO: Handle multiple assignment, other lvalues
+            # TODO: What about assignments without type annotations?
+            assert len(node.lvalues) == 1
+            lvalue = node.lvalues[0]
+            assert isinstance(lvalue, NameExpr)
+            assert self.type is not None  # Because self.is_class_body is True
+            del self.type.names[lvalue.name]
+        super().visit_assignment_stmt(node)
+
+    def visit_import_from(self, node: ImportFrom) -> None:
+        if node.assignments:
+            node.assignments = []
+        else:
+            if self.names:
+                # Reset entries in the symbol table. This is necessary since
+                # otherwise the semantic analyzer will think that the import
+                # assigns to an existing name instead of defining a new one.
+                for name, as_name in node.names:
+                    imported_name = as_name or name
+                    symnode = self.names[imported_name]
+                    symnode.kind = UNBOUND_IMPORTED
+                    symnode.node = None
+
+    def visit_import(self, node: Import) -> None:
+        if node.assignments:
+            node.assignments = []
+        else:
+            if self.names:
+                # Reset entries in the symbol table. This is necessary since
+                # otherwise the semantic analyzer will think that the import
+                # assigns to an existing name instead of defining a new one.
+                for name, as_name in node.ids:
+                    imported_name = as_name or name
+                    initial = imported_name.split('.')[0]
+                    symnode = self.names[initial]
+                    symnode.kind = UNBOUND_IMPORTED
+                    symnode.node = None
+
+    def visit_name_expr(self, node: NameExpr) -> None:
+        # Global assignments are processed in semantic analysis pass 1, and we
+        # only want to strip changes made in passes 2 or later.
+        if not (node.kind == GDEF and node.is_new_def):
+            self.strip_ref_expr(node)
+
+    def visit_member_expr(self, node: MemberExpr) -> None:
+        self.strip_ref_expr(node)
+        if self.is_duplicate_attribute_def(node):
+            # This is marked as an instance variable definition but a base class
+            # defines an attribute with the same name, and we can't have
+            # multiple definitions for an attribute. Defer to the base class
+            # definition.
+            if self.type is not None:
+                del self.type.names[node.name]
+            node.is_inferred_def = False
+            node.def_var = None
+        super().visit_member_expr(node)
+
+    def is_duplicate_attribute_def(self, node: MemberExpr) -> bool:
+        if not node.is_inferred_def:
+            return False
+        assert self.type is not None, "Internal error: Member defined outside class"
+        if node.name not in self.type.names:
+            return False
+        return any(info.get(node.name) is not None for info in self.type.mro[1:])
+
+    def strip_ref_expr(self, node: RefExpr) -> None:
+        node.kind = None
+        node.node = None
+        node.fullname = None
+
+    def visit_call_expr(self, node: CallExpr) -> None:
+        node.analyzed = None
+        super().visit_call_expr(node)
+
+    # TODO: handle more node types
+
+
+def is_self_member_ref(memberexpr: MemberExpr) -> bool:
+    """Does memberexpr refer to an attribute of self?"""
+    # TODO: Merge with is_self_member_ref in semanal.py.
+    if not isinstance(memberexpr.expr, NameExpr):
+        return False
+    node = memberexpr.expr.node
+    return isinstance(node, Var) and node.is_self
+
+
+@contextlib.contextmanager
+def nothing() -> Iterator[None]:
+    yield
