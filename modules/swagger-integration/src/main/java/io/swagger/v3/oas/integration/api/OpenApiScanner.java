92ed0405e1fd699e19f3ae216b8dc5c1bca95689
tdurieux <durieuxthomas@gmail.com>
2019-04-06 11:51:38 +0000

    Buggy version of swagger-api-swagger-core-299605362

---
+++ modules/swagger-integration/src/main/java/io/swagger/v3/oas/integration/api/OpenApiScanner.java
@@ -0,0 +1,15 @@
+package io.swagger.v3.oas.integration.api;
+
+import java.util.Map;
+import java.util.Set;
+
+public interface OpenApiScanner {
+
+    void setConfiguration(OpenAPIConfiguration openApiConfiguration);
+
+    Set<Class<?>> classes();
+
+    Map<String, Object> resources();
+
+
+}
