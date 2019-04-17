71a21eef1eb18b1aa82395f5a5a8a3d8583ab102
tdurieux <durieuxthomas@gmail.com>
2019-04-17 13:16:04 +0000

    Buggy version of checkstyle-checkstyle-74845220

---
+++ src/test/resources/com/puppycrawl/tools/checkstyle/coding/InputEqualsAvoidNull.java
@@ -0,0 +1,230 @@
+package com.puppycrawl.tools.checkstyle.coding;
+
+public class InputEqualsAvoidNull {
+
+    public boolean equals(Object o) {
+    return false;
+    }
+    // antoher comment
+    /**
+     * methods that should get flagged
+     * @return
+     */
+    public void flagForEquals() {
+
+        Object o = new Object();
+        String s = "pizza";
+
+        o.equals("hot pizza")/*comment test*/;
+
+        o.equals(s = "cold pizza");
+
+        o.equals(((s = "cold pizza")));
+
+        o.equals("cheese" + "ham" + "sauce");
+
+        o.equals(("cheese" + "ham") + "sauce");
+
+        o.equals((("cheese" + "ham")) + "sauce");
+    }
+
+    /**
+     * methods that should get flagged
+     */
+    public void flagForEqualsIgnoreCase() {
+        String s = "pizza";
+
+        s.equalsIgnoreCase("hot pizza");
+
+        s.equalsIgnoreCase(s = "cold pizza");
+
+        s.equalsIgnoreCase(((s = "cold pizza")));
+
+        s.equalsIgnoreCase("cheese" + "ham" + "sauce");
+
+        s.equalsIgnoreCase(("cheese" + "ham") + "sauce");
+
+        s.equalsIgnoreCase((("cheese" + "ham")) + "sauce");
+    }
+
+    /**
+     * methods that should get flagged
+     */
+    public void flagForBoth() {
+        Object o = new Object();
+        String s = "pizza";
+
+        o.equals("hot pizza");
+
+        o.equals(s = "cold pizza");
+
+        o.equals(((s = "cold pizza")));
+
+        o.equals("cheese" + "ham" + "sauce");
+
+        o.equals(("cheese" + "ham") + "sauce");
+
+        o.equals((("cheese" + "ham")) + "sauce");
+
+        s.equalsIgnoreCase("hot pizza");
+
+        s.equalsIgnoreCase(s = "cold pizza");
+
+        s.equalsIgnoreCase(((s = "cold pizza")));
+
+        s.equalsIgnoreCase("cheese" + "ham" + "sauce");
+
+        s.equalsIgnoreCase(("cheese" + "ham") + "sauce");
+
+        s.equalsIgnoreCase((("cheese" + "ham")) + "sauce");
+    }
+
+
+    /**
+     * methods that should not get flagged
+     *
+     * @return
+     */
+    public void noFlagForEquals() {
+        Object o = new Object();
+        String s = "peperoni";
+
+        o.equals(s += "mushrooms");
+
+        (s = "thin crust").equals("thick crust");
+
+        (s += "garlic").equals("basil");
+
+        ("Chicago Style" + "NY Style").equals("California Style" + "Any Style");
+
+        equals("peppers");
+
+        "onions".equals(o);
+
+        o.equals(new Object());
+
+        o.equals(equals(o));
+
+        equals("yummy");
+
+        new Object().equals("more cheese");
+
+        InputEqualsAvoidNullOutter outter = new InputEqualsAvoidNullOutter();
+
+        outter.new InputEqualsAvoidNullInner().equals("eat pizza and enjoy inner classes");
+    }
+
+    /**
+     * methods that should not get flagged
+     */
+    public void noFlagForEqualsIgnoreCase() {
+        String s = "peperoni";
+        String s1 = "tasty";
+
+        s.equalsIgnoreCase(s += "mushrooms");
+
+        s1.equalsIgnoreCase(s += "mushrooms");
+
+        (s = "thin crust").equalsIgnoreCase("thick crust");
+
+        (s += "garlic").equalsIgnoreCase("basil");
+
+        ("Chicago Style" + "NY Style").equalsIgnoreCase("California Style" + "Any Style");
+
+        "onions".equalsIgnoreCase(s);
+
+        s.equalsIgnoreCase(new String());
+
+        s.equals(s1);
+
+        new String().equalsIgnoreCase("more cheese");
+
+    }
+
+    public void noFlagForBoth() {
+        Object o = new Object();
+        String s = "peperoni";
+        String s1 = "tasty";
+
+        o.equals(s += "mushrooms");
+
+        (s = "thin crust").equals("thick crust");
+
+        (s += "garlic").equals("basil");
+
+        ("Chicago Style" + "NY Style").equals("California Style" + "Any Style");
+
+        equals("peppers");
+
+        "onions".equals(o);
+
+        o.equals(new Object());
+
+        o.equals(equals(o));
+
+        equals("yummy");
+
+        new Object().equals("more cheese");
+
+        InputEqualsAvoidNullOutter outter = new InputEqualsAvoidNullOutter();
+
+        outter.new InputEqualsAvoidNullInner().equals("eat pizza and enjoy inner classes");
+
+        s.equalsIgnoreCase(s += "mushrooms");
+
+        s1.equalsIgnoreCase(s += "mushrooms");
+
+        (s = "thin crust").equalsIgnoreCase("thick crust");
+
+        (s += "garlic").equalsIgnoreCase("basil");
+
+        ("Chicago Style" + "NY Style").equalsIgnoreCase("California Style" + "Any Style");
+
+        "onions".equalsIgnoreCase(s);
+
+        s.equalsIgnoreCase(new String());
+
+        s.equals(s1);
+
+        new String().equalsIgnoreCase("more cheese");
+
+    }
+
+}
+
+class InputEqualsAvoidNullOutter {
+    public class InputEqualsAvoidNullInner {
+    	public boolean equals(Object o) {
+    	    return true;
+    	}
+    }
+}
+
+class MyString {
+    public boolean equals() {
+        return true;
+    }
+    
+    public boolean equals(String s1) {
+        return true;
+    }
+    
+    public boolean equalsIgnoreCase() {
+        return true;
+    }
+    
+    public boolean equalsIgnoreCase(String s1) {
+        return true;
+    }
+    
+    private String pizza;
+    
+    public void main() {
+        MyString myString = new MyString();
+        myString.equals();
+        myString.equals("what");
+        myString.equalsIgnoreCase();
+        myString.equalsIgnoreCase("what");
+        myString.equals(this.pizza = "cold pizza");
+    }
+}
