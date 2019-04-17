f90da2870d86480b871fce71a224f44c8187dbcb
tdurieux <durieuxthomas@gmail.com>
2019-04-17 08:46:35 +0000

    Buggy version of kairosdb-kairosdb-160772310

---
+++ src/main/java/org/kairosdb/core/datastore/QueryCallback.java
@@ -0,0 +1,20 @@
+package org.kairosdb.core.datastore;
+
+import java.io.IOException;
+import java.util.Map;
+import org.kairosdb.core.DataPoint;
+
+/**
+ Created with IntelliJ IDEA.
+ User: bhawkins
+ Date: 10/18/13
+ Time: 1:02 PM
+ To change this template use File | Settings | File Templates.
+ */
+public interface QueryCallback
+{
+	public void addDataPoint(DataPoint datapoint) throws IOException;
+	
+	public void startDataPointSet(String dataType, Map<String, String> tags) throws IOException;
+	public void endDataPoints() throws IOException;
+}
