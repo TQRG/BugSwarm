e75d211dfdbb742d5878aa0cca8f0081fec130fa
tdurieux <durieuxthomas@gmail.com>
2019-04-17 10:21:56 +0000

    Passed version of SonarSource-sonar-java-382295561

--- check-license-compliance.sh
+++ check-license-compliance.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 set -euo pipefail
+
+# See https://xtranet.sonarsource.com/display/DEV/Open+Source+Licenses
 
 mvn org.codehaus.mojo:license-maven-plugin:aggregate-add-third-party -Dlicense.aggregateMissingLicensesFile=$(pwd)/missing-dep-licenses.properties -DuseMissingFile
